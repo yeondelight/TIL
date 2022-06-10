cc0cd9ed93c36c72eabc2e255469e866a751b542
yeondelight <hihi5v@hansung.ac.kr>
2022-06-10 21:21:00 +0900

    0610-2

--- 0610/src/Main.java
+++ 0610/src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -171,7 +173,176 @@ public class Main {
 		System.out.println(sb);
 	}
 	
+	public void sol_2869() throws Exception {
+		// ready
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				
+		// scan
+		StringTokenizer st = new StringTokenizer(br.readLine());
+			
+		// cal
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int v = Integer.parseInt(st.nextToken());
+	
+		v -= a;
+		int count;
+		if ((v % ( a - b )) == 0)	count = (v / ( a - b )) + 1;
+		else						count = (v / ( a - b )) + 2;
+		
+		// print
+		System.out.println(count);
+	}
+
+	public void sol_7568() throws Exception {
+		// ready
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		// scan
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		
+		// cal
+		int i, j;
+		int rank[] = new int[n];
+		int weight[] = new int[n];
+		int height[] = new int[n];
+		
+		for (i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			weight[i] = Integer.parseInt(st.nextToken());
+			height[i] = Integer.parseInt(st.nextToken());
+			rank[i] = 1;
+		}
+	
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; j++) {
+				if ( i == j )	continue;
+				if ((weight[i] < weight[j]) && (height[i] < height[j])) {
+					rank[i]++;
+				}
+			}
+			sb.append(rank[i]).append(' ');
+		}
+		
+		// print
+		System.out.println(sb);
+	}
+
+	public void sol_1181() throws Exception {
+		// ready
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		// scan
+		int n = Integer.parseInt(br.readLine());
+		
+		// cal
+		int i;
+		String word;
+		List<String> words = new ArrayList<String>();
+		
+		for (i = 0; i < n; i++) {
+			word = br.readLine();
+			if (words.contains(word))	continue;
+			else						words.add(word);
+		}
+		
+		Collections.sort(words, new Comparator<String>() {
+			public int compare (String s1, String s2) {
+				if (s1.length() > s2.length())		return 1;
+				else if (s1.length() < s2.length())	return -1;
+				else								return s1.compareTo(s2);
+			}
+		});
+		
+		// print
+		for (i = 0; i < words.size(); i++) {
+			sb.append(words.get(i)).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+
+	public void sol_1427() throws Exception {
+		// ready
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		// scan
+		int n = Integer.parseInt(br.readLine());
+		
+		// cal
+		List<Integer> nums = new ArrayList<Integer>();
+		
+		do {
+			nums.add(n % 10);
+			n /= 10;
+		} while(n > 0);
+		
+		Collections.sort(nums);
+		Collections.reverse(nums);
+		
+		// print
+		for (int i = 0; i < nums.size(); i++) {
+			sb.append(nums.get(i));
+		}
+		
+		System.out.println(sb);
+	}
+	
+	public void sol_11650() throws Exception {
+		// ready
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		// scan
+		int n = Integer.parseInt(br.readLine());
+		
+		// cal
+		int i, x, y;
+		List<Point> points = new ArrayList<Point>();
+		
+		for (i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			x = Integer.parseInt(st.nextToken());
+			y = Integer.parseInt(st.nextToken());
+			points.add(new Point(x, y));
+		}
+		
+		Collections.sort(points, new Comparator<Point>() {
+			public int compare (Point p1, Point p2) {
+				if (p1.x > p2.x)		return 1;
+				else if (p1.x < p2.x)	return -1;
+				else {
+					if (p1.y > p2.y)		return 1;
+					else if (p1.y < p2.y)	return -1;
+					else					return 0;
+				}
+			}
+		});
+		
+		// print
+		for (i = 0; i < points.size(); i++) {
+			sb.append(points.get(i).x).append(' ').append(points.get(i).y).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+	
+	class Point {
+		int x;
+		int y;
+		
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
 	public static void main(String [] args) throws Exception{
-		new Main().sol_2751();
+		new Main().sol_11650();
 	}
 }
