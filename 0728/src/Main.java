95f45d64b08faba1892fb07ff9e89a6d0d1c1fd5
yeondelight <hihi5v@hansung.ac.kr>
2022-07-28 14:45:12 +0900

    0728-2

--- 0728/src/Main.java
+++ 0728/src/Main.java
@@ -72,7 +72,45 @@ public class Main {
 		System.out.println(sb);
 	}
 	
+	Map<Long, Long> fibo;
+	
+	public long conquer(long N) {
+		if (N == 0)	return 0;
+		if (N == 1)	return 1;
+		if (fibo.containsKey(N))	return fibo.get(N) % 1000000007;
+
+		long mod = N % 2;
+		long N0, N1;
+		N0 = conquer(N/2);
+		if (!fibo.containsKey(N/2)) {
+			fibo.put(N/2, N0);
+		}
+		if (mod == 0) {
+			N1 = conquer(N/2-1) % 1000000007;
+			if (!fibo.containsKey(N/2-1)) {
+				fibo.put(N/2-1, N1);
+			}
+			return N0 * (N0 + 2*N1 % 1000000007) % 1000000007;
+		} else {
+			N1 = conquer(N/2+1) % 1000000007;
+			if (!fibo.containsKey(N/2-1)) {
+				fibo.put(N/2+1, N1);
+			}
+			return (N0 * N0 % 1000000007 + N1 * N1 % 1000000007) % 1000000007;
+		}
+	}
+	
+	public void sol_11444() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long N = Long.parseLong(br.readLine());
+		fibo = new HashMap<>();
+		fibo.put((long)0, (long)0);
+		fibo.put((long)1, (long)1);
+		
+		System.out.println(conquer(N));
+	}
+	
 	public static void main(String[] args) throws Exception {
-		new Main().sol_1865();
+		new Main().sol_11444();
 	}
 }
