e0b44c30d84208f70fa6e54d6a4c404b84181869
yeondelight <hihi5v@hansung.ac.kr>
2022-12-13 13:09:03 +0900

    1213

---
+++ 1213/src/Main.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static int N;
+	public static int ans;
+	public static int part;
+	public static int[][] S;
+	public static boolean[] team;
+	public static boolean[] check;
+	
+	public int checkDiff() {
+		// cal team's energy.
+		int start = 0;
+		int link = 0;
+		for (int i = 1; i <= N; i++) {
+			for (int j = i + 1; j <= N; j++) {
+				if (team[i] && team[j]) {
+					start += (S[i][j] + S[j][i]);
+				}
+				else if (!team[i] && !team[j]) {
+					link += (S[i][j] + S[j][i]);
+				}
+			}
+		}
+		return Math.abs(start - link);
+	}
+	
+	public void backtracking(int depth, int start) {
+		if (depth == part) {
+			ans = Math.min(ans, checkDiff());
+			return;
+		}
+		for (int i = depth; i < N; i++) {
+			if (!check[i]) {
+				check[i] = true;
+				team[i+1] = true;
+				backtracking(i+1, start+1);
+				team[i+1] = false;
+				check[i] = false;
+			}
+		}
+	}
+	
+	public void sol_15661() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		S = new int[N+1][N+1];
+		for (int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				S[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		ans = Integer.MAX_VALUE;
+		for (int i = 1; i < N; i++) {
+			part = i;
+			team = new boolean[N+1];
+			check = new boolean[N+1];
+			backtracking(0, 0);
+		}
+		
+		System.out.println(ans);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		new Main().sol_15661();
+	}
+}
