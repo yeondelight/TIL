128d90645f17f08ace35053dd30ebd0fbc90f13d
yeondelight <hihi5v@hansung.ac.kr>
2022-12-22 16:58:16 +0900

    1222

---
+++ 1222/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	
+	public void sol_25083() throws Exception {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+	
+	public static void main(String[] args) throws Exception {
+		new Main().sol_25083();
+	}
+
+}
