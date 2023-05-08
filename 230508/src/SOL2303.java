a9ee33b75fc186b5a974cb8d1522eb354a35ec87
yeondelight <hihi5v@naver.com>
2023-05-08 22:00:25 +0900

    230508

--- 230508/src/SOL2303.java
+++ 230508/src/SOL2303.java
@@ -5,11 +5,12 @@ import java.util.StringTokenizer;
 class SOL2303 {
 
 	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static int SIZE = 5;
+	public static int CARDSIZE = 5;
+	public static int CHOOSESIZE = 3;
 	
 	public int N;
 	public int[][] card;
-	
+	public int[] value;
 	public int ans;
 	
 	public void run() throws Exception {
@@ -21,21 +22,41 @@ class SOL2303 {
 	public void getInput() throws Exception {
 		N = Integer.parseInt(br.readLine());
 		
-		card = new int[N][SIZE];
-		for (int i = 0; i < N; i++) {
+		card = new int[N+1][CARDSIZE];
+		for (int i = 1; i <= N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < SIZE; j++) {
+			for (int j = 0; j < CARDSIZE; j++) {
 				card[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 	}
 	
 	public void solution() {
+		value = new int[N+1];
+		for (int i = 1; i <= N; i++) {
+			backtracking(i, -1, 0, 0);
+		}
 		
+		for (int i = 1; i <= N; i++) {
+			if (value[ans] <= value[i]) {
+				ans = i;
+			}
+		}
 	}
 	
 	public void print() {
 		System.out.println(ans);
 	}
 
+	// backtracking으로 가장 큰 일의 자리 수를 찾는다.
+	public void backtracking(int n, int idx, int sum, int depth) {
+		if (depth == CHOOSESIZE) {
+			System.out.println(n + " : " + sum);
+			value[n] = Math.max(value[n], sum%10);
+			return;
+		}
+		for (int i = idx+1; i < CARDSIZE; i++) {
+			backtracking(n, i, sum + card[n][i], depth+1);
+		}
+	}
 }
